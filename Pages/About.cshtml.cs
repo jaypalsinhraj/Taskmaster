7eb8db9f96dd7f1c52e40c7abf97135020349add
jaypalsinhraj <link.jaypalraj@gmail.com>
2025-12-20 23:30:25 +0000

    Corrected the stuff on About

---
+++ Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TaskMaster.Pages
+{
+  public class AboutModel : PageModel
+  {
+    public void OnGet()
+    {
+      // This runs when the page is requested
+    }
+  }
+}
--- obj/Debug/net8.0/TaskMaster.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskMaster.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskMaster")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f79658777b3281c3870bfb7515cd4ca355f673")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskMaster")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskMaster")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
